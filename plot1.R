66abd878a0e08caab097b446a436574c20e430e1
fieevrasi <eeva.rasi@yahoo.com>
2017-07-17 12:35:33 +0300

    Small changes

--- plot1.R
+++ plot1.R
@@ -7,9 +7,6 @@ DT_full <- read.table("household_power_consumption.txt", header=TRUE, sep=";", n
 # Subset the data
 DT_sub <- DT_full[DT_full$Date %in% c("1/2/2007","2/2/2007") ,] 
 
-# Datetime
-datetime <- strptime(paste(DT_sub$Date, DT_sub$Time, sep=" "), "%d/%m/%Y %H:%M:%S")  
-
 # Global active power
 globalActivePower <- as.numeric(DT_sub$Global_active_power)
 
